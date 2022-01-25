4dd7dbe1336e007246eebae252279206c5bec85f
jinyunjae <jinyun3075@naver.com>
2022-01-25 17:25:58 +0900

    '백준 다시풀기'

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,57 @@
-import java.io.IOException;
-import java.util.PriorityQueue;
 import java.util.*;
-
 public class Main {
-        public static void main(String[] args) throws IOException {
-            Scanner sc = new Scanner(System.in);
-            StringBuilder sb = new StringBuilder();
-
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 2]*2 + dp[i - 1]) % 10007 ;
         }
-    class Solution {
-        public int[] solution(int[] numbers) {
-            PriorityQueue<Integer> arr = new PriorityQueue<>();
-            HashSet<Integer> set = new HashSet<>();
-            for(int i=0;i<numbers.length-1;i++){
-                for(int j=i+1;j<numbers.length;j++){
-                    set.add(numbers[i]+numbers[j]);
-                }
-            }
-            set.forEach((e)->arr.add(e));
-            int[] answer = new int[arr.size()];
-            for(int i =0;i<answer.length;i++){
-                answer[i]=arr.poll();
-            }
-            return answer;
-        }
+        System.out.print(dp[n]);
     }
-        static class player{
-            private String name;
-            private int score;
-
-            public player(String name,int score){
-                this.name=name;
-                this.score=score;
-            }
-
-            @Override
-            public String toString() {
-                return "player{" +
-                        "name='" + name + '\'' +
-                        ", score=" + score +
-                        '}';
-            }
-
-        }
-
 }
+/* 2+n 타일링2 */
+//public static void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//    int n = sc.nextInt();
+//    int[] dp = new int[n + 1];
+//    dp[0] = 1;
+//    dp[1] = 1;
+//    for (int i = 2; i <= n; i++) {
+//        dp[i] = (dp[i - 2]*2 + dp[i - 1]) % 10007 ;
+//    }
+//    System.out.print(dp[n]);
+//}
+/* 2+n 타일링 */
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int[] dp = new int[n + 1];
+//        dp[0] = 1;
+//        dp[1] = 1;
+//        for (int i = 2; i <= n; i++) {
+//            dp[i] = (dp[i - 2] + dp[i - 1]) % 10007 ;
+//        }
+//        System.out.print(dp[n]);
+//    }
+/* 1 만들기 */
+//    public static void main(String[] args) {
+//            Scanner sc = new Scanner(System.in);
+//            int x = sc.nextInt();
+//            int[] dp = new int[x+1];
+//            dp[1] = 0;
+//            for(int i = 2; i <= x ;i ++) {
+//                dp[i] = Integer.MAX_VALUE;
+//                if(i % 3 == 0 ) {
+//                    dp[i] = Math.min(dp[i],dp[i/3]+1);
+//                }
+//                if(i % 2 == 0) {
+//                    dp[i] = Math.min(dp[i],dp[i/2]+1);
+//                }
+//                dp[i] = Math.min(dp[i],dp[i-1]+1);
+//            }
+//            System.out.print(dp[x]);
+//    }
+
