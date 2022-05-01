84e0ed08e232e9344b11b730e1b18f78fdef8f9b
jinyunjae <jinyun3075@naver.com>
2022-05-01 13:44:37 +0900

    별 찍기

--- src/recursion/Bac10994.java
+++ src/recursion/Bac10994.java
@@ -1,6 +1,9 @@
-import java.util.*;
+package recursion;
 
-public class Main {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Bac10994 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
