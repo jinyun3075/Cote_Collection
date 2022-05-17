9fb409a4b61428208ce616b56225c1aef963bcb3
jinyunjae <jinyun3075@naver.com>
2022-05-17 12:15:54 +0900

    N과 M (11)

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,41 @@
 import java.util.*;
 
 class Main {
-    static List<String> resultList = new ArrayList<>();
-    static int[] input;
+    static int[] arr;
+    static int[] result;
     static int N;
+    static int M;
     static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-        int M = sc.nextInt();
-        input = new int[N];
+        M = sc.nextInt();
+        arr = new int[N];
         visit = new boolean[N];
-        for (int i = 0; i < N; i++) {
-            input[i] = sc.nextInt();
+        result = new int[N];
+        for(int i=0;i<N;i++) {
+            arr[i] = sc.nextInt();
         }
-        Arrays.sort(input);
-        dfs(M, "", 0);
+        Arrays.sort(arr);
+        dfs(0);
+        System.out.println(sb);
 
-        for(String print : resultList){
-            System.out.println(print.trim());
-        }
     }
-
-    public static void dfs(int count, String result,int idx) {
-        if (count == 0) {
-            if(!resultList.contains(result)){
-                resultList.add(result);
+    public static void dfs(int depth) {
+        if(depth==M) {
+            for(int i=0;i<M;i++) {
+                sb.append(result[i]).append(" ");
             }
+            sb.append("\n");
             return;
         }
-
-        for (int i = idx; i < N; i++) {
-            if(!visit[i]) {
-                visit[i] = true;
-                dfs(count-1,result+" "+input[i],i+1);
-                visit[i] = false;
+        int prev = -1;
+        for(int i=0;i<N;i++) {
+            if(prev!=arr[i]) {
+                prev = arr[i];
+                result[depth] = prev;
+                dfs(depth+1);
             }
         }
     }
