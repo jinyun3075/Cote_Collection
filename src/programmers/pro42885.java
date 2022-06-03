c839c38c326828866f2b941d56e55830148293c8
jinyunjae <jinyun3075@naver.com>
2022-06-03 19:28:55 +0900

    구명보트

---
+++ src/programmers/pro42885.java
@@ -0,0 +1,27 @@
+package programmers;
+
+public class pro42885 {
+    class Solution {
+        public int solution(int[] people, int limit) {
+            int answer = 0;
+            Arrays.sort(people);
+            int comIdx =0;
+            for(int i=people.length-1;i>=0;i--) {
+                if(comIdx>i) {
+                    break;
+                }
+                if(people[i]+people[comIdx]<=limit){
+                    comIdx++;
+                    answer++;
+                    continue;
+                }
+
+                answer++;
+            }
+            return answer;
+        }
+        public void logic(){
+
+        }
+    }
+}
