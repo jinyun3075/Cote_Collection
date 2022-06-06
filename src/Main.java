ba939831279d1e166d3aec179d97e9810254cd99
jinyunjae <jinyun3075@naver.com>
2022-06-06 10:39:58 +0900

    섬의 개수

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,53 @@
 import java.util.Scanner;
 
 class Main {
+    static int[][] board;
     static int N;
     static int M;
-    static int[][] map;
-    static boolean[][] direction;
-    static int max=0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
-
-        map = new int[N][M];
-        direction = new boolean[N][M];
-
-        for(int i=0;i<N;i++) {
-            String input = sc.next();
-            for(int j=0;j<M;j++) {
-                map[i][j] = input.charAt(j) - '0';
+        while(true) {
+            M = sc.nextInt();
+            N = sc.nextInt();
+            if(N==0||M==0) {
+                break;
             }
-        }
 
-        dfs(0,0);
-        System.out.println(max);
-    }
+            board = new int[N][M];
 
-    public static void dfs(int row, int cal) {
-        if(row>=N) {
-            logic();
-            return;
-        }
-        if(cal>=M) {
-            dfs(row+1,0);
-            return;
-        }
-
-        direction[row][cal] = true;
-        dfs(row, cal+1);
-        direction[row][cal] = false;
-        dfs(row, cal+1);
-    }
-
-    public static void logic() {
-        int temp =0;
-        int result =0;
-
-        for(int i=0;i<N;i++) {
-            temp = 0;
-            for(int j=0;j<M;j++) {
-                if(direction[i][j]) {
-                    temp *= 10;
-                    temp += map[i][j];
-                } else {
-                    result += temp;
-                    temp = 0;
+            for(int i=0;i<N;i++) {
+                for(int j=0;j<M;j++) {
+                    board[i][j] = sc.nextInt();
                 }
             }
-            result += temp;
-        }
-        for(int i=0;i<M;i++) {
-            temp = 0;
-            for(int j=0;j<N;j++) {
-                if(!direction[j][i]) {
-                    temp *= 10;
-                    temp += map[j][i];
-                }else {
-                    result += temp;
-                    temp = 0;
+            int result =0;
+            for(int i=0;i<N;i++) {
+                for(int j=0;j<M;j++) {
+                    if(board[i][j]==1) {
+                        dfs(i,j);
+                        result++;
+                    }
                 }
             }
-            result += temp;
+
+            System.out.println(result);
+        }
+    }
+
+    public static void dfs(int x, int y) {
+        if(0>y||0>x||x>=N||y>=M||board[x][y]==0) {
+            return;
         }
 
-        max = Math.max(max,result);
+        board[x][y] = 0;
+        dfs(x+1,y+1);
+        dfs(x+1,y-1);
+        dfs(x-1,y+1);
+        dfs(x-1,y-1);
+        dfs(x+1,y);
+        dfs(x-1,y);
+        dfs(x,y+1);
+        dfs(x,y-1);
+
     }
 }
