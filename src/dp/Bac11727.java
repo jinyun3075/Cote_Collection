7ecb22a68a23092c1c02d1b528197b7bdf199e29
PC1 <jinyun3075@naver.com>
2025-07-17 14:32:45 +0900

    DP 문제 모음

---
+++ src/dp/Bac11727.java
@@ -0,0 +1,16 @@
+package dp;
+import java.io.*;
+
+public class Bac11727 {
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        dp[0] = 1;
+        dp[1] = 1; 
+        for(int i = 2 ; i <= N ;i++){
+            dp[i] = (dp[i-2] * 2 + dp[i-1]) %10007;
+        }
+        System.out.println(dp[N]);
+    }
+}
